d0d7d9cb23c013a7fc9e4f1ee6104a64c8b5bf30
Júnior Chagas <junior chagas@oam.int>
2018-04-04 10:04:02 -0300

    first commit

---
+++ src/pages/feed/feed.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the FeedPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-feed',
+  templateUrl: 'feed.html',
+})
+export class FeedPage {
+  public nome_usuario:string = "Ivanaldo Junior";
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  public somaDoisNumeros():void{
+    alert(5+10);
+  }
+
+
+  ionViewDidLoad() {
+    
+  }
+
+}
